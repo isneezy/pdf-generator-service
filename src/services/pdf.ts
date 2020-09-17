ef118a04b22b3bc69d4fb6cae6dce6faeec63ba8
Ivan Vilanculo <isneezy@users.noreply.github.com>
2020-09-17 17:04:42 +0200

    feature github actions (#2)
    * configure eslint
    
    * update workflow
    
    * fix linting issues
    
    * update dependencies
    
    * attempt to fix conflicts with eslint rules
    
    * add prettier configuration
    
    * run ci tests
    
    * fix tests
    
    * restore jest configuration
    
    * cache node_modules
    
    * fix workflow
    
    * audit packages for security
    
    * update workflow add build step
    
    * add build

--- src/services/pdf.ts
+++ src/services/pdf.ts
@@ -1,5 +1,5 @@
-import {Browser} from "puppeteer";
-import {PdfOptions, pdfOptionsFactory} from "./PdfOptions";
+import { Browser } from 'puppeteer'
+import { PdfOptions, pdfOptionsFactory } from './PdfOptions'
 
 export const PAPER_FORMATS = ['A3', 'A4', 'A5', 'Legal', 'Letter', 'Tabloid']
 export const PAGE_ORIENTATIONS = ['portrait', 'landscape']
@@ -11,23 +11,23 @@ export class Pdf {
     this.browser = browser
   }
 
-  public async generate(options: PdfOptions) {
+  public async generate(options: PdfOptions): Promise<Buffer> {
     options = pdfOptionsFactory(options)
     const page = await this.browser.newPage()
     try {
-      await page.setContent(options.content, {waitUntil: 'networkidle2'})
+      await page.setContent(options.content, { waitUntil: 'networkidle2' })
       const pdf = await page.pdf({
         format: options.format,
         landscape: options.orientation == 'landscape',
-        printBackground: true
+        printBackground: true,
       })
       await page.close()
       return pdf
     } catch (e) {
       if (page) {
         await page.close()
-        throw new e
       }
+      throw new e()
     }
   }
 }
