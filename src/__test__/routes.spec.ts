a274bfe770c4dedc217f2fc73ddfcf5aa8470805
Ivan Vilanculo <vilanculoivan@gmail.com>
2020-09-21 15:22:45 +0200

    make app creation asynchronous

--- src/__test__/routes.spec.ts
+++ src/__test__/routes.spec.ts
@@ -1,16 +1,21 @@
 import request from 'supertest'
 import pkg from '../../package.json'
-import { app, container } from '../app'
+import createApp, { CreateAppReturnType } from '../app'
 import Puppeteer, { Browser } from 'puppeteer'
 
+let instance: CreateAppReturnType | undefined
+
+beforeAll(async () => {
+  instance = await createApp()
+})
+
 afterAll(async () => {
-  // cleanup resources
-  await container.resolve<Browser>('browser').close()
+  await instance?.container.resolve<Browser>('browser').close()
 })
 
 describe('GET /', () => {
   it('shows application name, description, version, author, repository, author and bugs on root path', (done) => {
-    const response = request(app).get('/')
+    const response = request(instance?.app).get('/')
     response.expect('Content-Type', /json/).expect(
       200,
       {
@@ -29,20 +34,11 @@ describe('GET /', () => {
 
 describe('POST /generate', () => {
   it('should generate and stream pdf file based on html input', async (done) => {
-    // dirty hack to fix missing dependency on ioc
-    const args: string[] = (process.env.PUPPETEER_ARGS || '').split(' ')
-    const browser = await Puppeteer.launch({ args })
-    try {
-      await request(app)
-        .post('/generate')
-        .send({ content: '<h2>Hello</h2>' })
-        .expect('Content-Type', /pdf/)
-        .expect(200)
-      await browser.close()
-    } catch (e) {
-      await browser.close()
-      throw e
-    }
+    await request(instance?.app)
+      .post('/generate')
+      .send({ content: '<h2>Hello</h2>' })
+      .expect('Content-Type', /pdf/)
+      .expect(200)
     done()
   })
 })
