35178d3fb03170f2bf1a5b8895c95bf83e88c589
Ivan Vilanculo <vilanculoivan@gmail.com>
2020-09-21 18:44:21 +0200

    add Pdf service tests

---
+++ src/services/__test__/Pdf.spec.ts
@@ -0,0 +1,54 @@
+import puppeteer, { PDFOptions } from 'puppeteer'
+import { Pdf } from '../pdf'
+import { mocked } from 'ts-jest/utils'
+import { PdfOptions, pdfOptionsFactory } from '../PdfOptions'
+
+const pageProto = {
+  setContent: jest.fn().mockImplementation(() => Promise.resolve()),
+  pdf: jest.fn().mockImplementation(() => Promise.resolve()),
+  close: jest.fn().mockImplementation(() => Promise.resolve()),
+}
+
+const browserProto = {
+  newPage: jest.fn().mockImplementation(async () => pageProto),
+}
+
+jest.mock('puppeteer', () => ({
+  launch: async () => browserProto,
+}))
+
+async function testPdfParam(
+  options: Partial<PdfOptions>,
+  pdfOptions: PDFOptions
+) {
+  browserProto.newPage.mockClear()
+  pageProto.close.mockClear()
+  pageProto.pdf.mockClear()
+
+  const mockedPuppeteer = mocked(puppeteer)
+  const browser = await mockedPuppeteer.launch()
+  const pdf = new Pdf(browser)
+  await pdf.generate(
+    pdfOptionsFactory(Object.assign({ content: '<h2>Hello</h2>' }, options))
+  )
+  expect(browserProto.newPage).toBeCalledTimes(1)
+  expect(pageProto.pdf).toBeCalledTimes(1)
+  expect(pageProto.close).toBeCalledTimes(1)
+  expect(pageProto.pdf).lastCalledWith(expect.objectContaining(pdfOptions))
+}
+
+describe('Pdf', () => {
+  it('initializes', async () => {
+    const mockedPuppeteer = mocked(puppeteer)
+    const browser = await mockedPuppeteer.launch()
+    expect(new Pdf(browser)).toBeInstanceOf(Pdf)
+  })
+
+  it('generates portrait pdf', async () => {
+    await testPdfParam({ orientation: 'portrait' }, { landscape: false })
+  })
+
+  it('generates landscape pdf', async () => {
+    await testPdfParam({ orientation: 'landscape' }, { landscape: true })
+  })
+})
