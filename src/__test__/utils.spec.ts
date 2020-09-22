90ee657fd2b2b01a9446fefd3421c19475018575
Ivan Vilanculo <vilanculoivan@gmail.com>
2020-09-22 17:20:37 +0200

    update tests

--- src/__test__/utils.spec.ts
+++ src/__test__/utils.spec.ts
@@ -8,4 +8,14 @@ describe('utils.ts', () => {
     const expected = `<div style="margin: 0 1.9cm 0 1.9cm; font-size: 8px"><h2>hello</h2></div>`
     expect(compileHeaderOrFooterTemplate(template, options)).toBe(expected)
   })
+
+  it('should make the context available to the template', () => {
+    const options = pdfOptionsFactory({
+      content: '<h1>hello</h1>',
+      context: { name: 'PDF Express' },
+    })
+    const template = '<h2>hello {{ name }}</h2>'
+    const expected = `<div style="margin: 0 1.9cm 0 1.9cm; font-size: 8px"><h2>hello PDF Express</h2></div>`
+    expect(compileHeaderOrFooterTemplate(template, options)).toBe(expected)
+  })
 })
--- src/utils.ts
+++ src/utils.ts
@@ -9,7 +9,7 @@ export function compileHeaderOrFooterTemplate(
   // Currently the header and footer on chromium does not inherit the document styles.
   // This issue causes them to render with font-size: 0 and causes them to render on the edge of the page
   // has a dirty fix we will force it to be rendered with some sensible defaults and it can be override by setting an inner style.
-  const printTemplate = `<div style="margin: 0 ${options.margin?.right}} 0 ${options.margin?.left}}; font-size: 8px">${template}</div>`
+  const printTemplate = `<div style="margin: 0 ${options.margin?.right} 0 ${options.margin?.left}; font-size: 8px">${template}</div>`
   if (options.context) {
     const context = {
       ...options.context,
